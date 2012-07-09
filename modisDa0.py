ac2ecba3f431eb3ebaf1dcc62ad2836461f1650c
Jose Gomez-Dans <jgomezdans@gmail.com>
2012-07-09 23:07:02 +0100

    added themodidada0.py script. Should be used instead of modisda1, but docs have yet to be updated

--- modisDa0.py
+++ modisDa0.py
@@ -1,32 +1,40 @@
 import numpy as np
-import eoldas
+import eoldaslib as eoldas
 import sys,tempfile
 import os
 
-gamma = 100/np.sqrt(28.)
+gamma = 50
 model_order = 1
-BRF = 'data/brdf_WW_1_A_1.kernelFiltered.dat'
+dayStep  = 2
+BRF = 'data/modis/brdf_WW_1_A_1.kernelFiltered.dat'
 
 names = 'gamma_time,xlai, xhc,  rpl,  xkab, scen, xkw, xkm,   xleafn, xs1,xs2,xs3,xs4,lad'
-default = '%f,0.99,5,0.01,0.99,0.001,0.99,0.35,1.5,1,0.001,0,0,5'%gamma
+defaultp = '0.99,0.01,0.01,0.99,0.001,0.99,0.35,1.0,1,0.001,0.001,0.001,5'
+default = '%f,%s'%(gamma,defaultp)
+priorsd = ''.join(['0.33'] + [',0.33']*(len(names.split(','))-2))
 
 #default = '%f,0.99,5,0.01,0.5,0.001,0.5,0.35,1.5,1,0.001,0,0,5'%gamma
 
 cmd = 'eoldas ' +  \
-                '--conf=config_files/eoldas_config.conf  ' + \
-                '--conf=config_files/semid_default.conf ' + \
+                '--conf=system_confs/eoldas_config.conf  ' + \
+                '--conf=confs/semid_default.conf ' + \
                 '--parameter.x.default=%s '%default + \
                 '--parameter.solve=0,1,0,0,1,1,1,1,1,1,1,0,0,0 ' +\
                 '--logfile=logs/file.log ' + \
-                '--parameter.limits=[[1,365,28]] ' +\
+                '--parameter.limits=[[1,365,%d]] '%dayStep +\
                 '--parameter.result.filename=output/gamma%f/state.dat '%gamma + \
                 '--operator.obs.y.result.filename=output/gamma%f/obs.dat '%gamma + \
                 '--operator.obs.y.state=%s '%BRF + \
-                '--operator.modelt.rt_model.model_order=%d '%model_order + \
+                '--operator.prior.y.sd=%s '%priorsd + \
+                '--operator.prior.y.state=%s '%defaultp + \
+                ' --operator.modelt.rt_model.model_order=%d '%model_order + \
                 '--calc_posterior_unc'
 
+# '--parameter.x.state=output/gamma%f/state.dat '%gamma + \
+# '--conf=confs/semid_default_state.conf' + \
+
 # initialise eoldas
-m = eoldas.eoldas(cmd)
+self = eoldas.eoldas(cmd)
 # solve DA
-m.solve(write=True)
+self.solve(write=True)
 
